d1c8319887a6467c8159ee0adc96437e7802c043
Nicolas Vibert <nicolasvibert@nvibert-a01.vmware.com>
2020-09-02 16:47:24 +0100

    New SDDC deploy

--- main.tf
+++ main.tf
@@ -17,9 +17,9 @@ data "vmc_customer_subnets" "my_subnets" {
 }
 
 resource "vmc_sddc" "sddc_1" {
-  sddc_name           = "my_SDDC_runvmc"
+  sddc_name           = "my_SDDC_runvmc_github"
   vpc_cidr            = var.sddc_mgmt_subnet
-  num_host            = 4
+  num_host            = 3
   provider_type       = "ZEROCLOUD"
   region              = data.vmc_customer_subnets.my_subnets.region
   vxlan_subnet        = var.sddc_default
