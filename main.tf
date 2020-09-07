381f390bd2d34442b36b8dca870ad6fe9c3ec59c
Nico Vibert <nvibert@vmware.com>
2020-09-07 20:46:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "vmc_customer_subnets" "my_subnets" {
 }
 
 resource "vmc_sddc" "sddc_1" {
-  sddc_name           = "my_SDDC_runvmc_github_test"
+  sddc_name           = "my_SDDC_runvmc_github"
   vpc_cidr            = var.sddc_mgmt_subnet
   num_host            = 3
   provider_type       = "ZEROCLOUD"
