8b995a260061b4cedad6f3f7b636e1e0ef019e0a
Nico Vibert <nvibert@vmware.com>
2021-01-13 17:04:38 +0000

    Update main.tf
    Testing TF Cloud

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "vmc_customer_subnets" "my_subnets" {
 }
 
 resource "vmc_sddc" "sddc_1" {
-  sddc_name           = "my_SDDC_runvmc"
+  sddc_name           = "my_SDDC_runvmc_name_change"
   vpc_cidr            = var.sddc_mgmt_subnet
   num_host            = 3
   provider_type       = "ZEROCLOUD"
